20d5f7a7639d95fa67c4f4b45f78adf65c624d78
dudesoamazing <marvinogah@gmail.com>
2026-05-07 10:43:35 +0100

    chore: enforce DM Sans typography across Storybook UI and root application

--- .storybook/fmdqTheme.ts
+++ .storybook/fmdqTheme.ts
@@ -16,7 +16,7 @@ export default create({
   appBorderRadius: 4,
 
   // Typography
-  fontBase: '"DM Sans", "Inter", "Helvetica", "Arial", sans-serif',
+  fontBase: '"DM Sans", "Helvetica", "Arial", sans-serif',
   fontCode: 'monospace',
 
   // Text colors
