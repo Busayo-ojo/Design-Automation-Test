2eb297110dc6752412b2e0b8e3a1a61dc8450df3
Busayo Ojo <ojobusayo99@gmail.com>
2026-05-25 15:28:59 +0100

    Welcome Page

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,11 +5,12 @@ const config: StorybookConfig = {
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  "staticDirs": ["../public"],
   "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@chromatic-com/storybook",
+    "@storybook/addon-vitest",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+    "@storybook/addon-onboarding"
   ],
   "framework": "@storybook/react-vite"
 };
